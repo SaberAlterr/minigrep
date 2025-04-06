8b3eec13d92b6261abbad06f2ca61383fb89d788
Cheng Zong <saberalter@hust.edu.cn>
2025-04-06 17:11:33 +0800

    The basic struct is done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 use std::env;
+use minigrep::Config;
+use minigrep::run;
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+    let config= Config::build(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        std::process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    if let Err(e) = run(config) {
+        eprintln!("Application error: {}", e);
+        std::process::exit(1);
+    }
 }
